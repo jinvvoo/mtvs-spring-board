c1eecccb7716449e3b46793bbf4e20ca0e80aeda
jinvvoo <jinvvookim@gmail.com>
2023-06-26 14:26:24 +0900

    board-1

---
+++ mtvs-reserve-test/src/main/java/com/ohgiraffers/mtvsreservetest/MainController.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.mtvsreservetest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+
+    @RequestMapping(value = {"/"})
+    public String main(){
+
+        return "main";
+    }
+}
---
+++ mtvs-reserve-test/src/main/java/com/ohgiraffers/mtvsreservetest/board/BoardController.java
@@ -0,0 +1,20 @@
+package com.ohgiraffers.mtvsreservetest.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/board")
+    public String list() {
+        return "board/list.html";
+    }
+
+    @GetMapping("/post")
+    public String post() {
+        return "board/post.html";
+    }
+
+
+}
