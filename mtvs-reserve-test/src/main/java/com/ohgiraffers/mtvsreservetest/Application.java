3a737d2104bd8303cc9a39985f19d6b58f17dfb7
jinvvoo <jinvvookim@gmail.com>
2023-06-27 16:12:10 +0900

    (6) 수정 기능 구현 완료

--- mtvs-reserve-test/src/main/java/com/ohgiraffers/mtvsreservetest/Application.java
+++ mtvs-reserve-test/src/main/java/com/ohgiraffers/mtvsreservetest/Application.java
@@ -2,7 +2,9 @@ package com.ohgiraffers.mtvsreservetest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,5 +12,10 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
 }
-// board-6 생성
+// board-6 수정기능
